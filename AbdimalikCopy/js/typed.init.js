956adc4b25636f5f7651389eebf00131e2104d03
abdimalikmoha <102222436+abdimalikmoha@users.noreply.github.com>
2022-05-31 20:38:40 +0300

    Added files via upload

---
+++ AbdimalikCopy/js/typed.init.js
@@ -0,0 +1,10 @@
+
+// Typed Text
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 200,
+    loop: true,
+    strings: [
+        'Frontend Developer', 'Backend Developer', 'UI/UX Designer','Graphics Designer', 'Mobile Developer', 'Software Developer'
+    ],
+});
